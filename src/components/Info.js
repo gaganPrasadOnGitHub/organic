6097a4a263e3d792ba8a13de9daf885d484d3e69
gaganPrasadOnGitHub <writetogaganprasad@gmail.com>
2023-11-16 13:59:24 +0530

    Update - credit info class

--- src/components/Info.js
+++ src/components/Info.js
@@ -7,17 +7,17 @@ const Info = () => {
   return (
     <div className="info_container">
       <h2>{translations[selectedLanguage].credits}</h2>
-      <p className="info">Images by jcomp on Freepik</p>
-      <p className="info">Images by topntp26 on Freepik</p>
-      <p className="info">Images by pch.vector on Freepik</p>
-      <p className="info">Images by mrsiraphol on Freepik</p>
-      <p className="info">Images by Vectonauta on Freepik</p>
-      <p className="info">Images by artbutenkov on Freepik</p>
-      <p className="info">Images by Racool_studio on Freepik</p>
-      <p className="info">Images by ededchechine on Freepik</p>
-      <p className="info">Images by chandlervid85 on Freepik</p>
-      <p className="info">Images by azerbaijan_stockers on Freepik</p>
-      <p className="info">Images by asier_relampagoestudio on Freepik</p>
+      <p className="info_item">Images by jcomp on Freepik</p>
+      <p className="info_item">Images by topntp26 on Freepik</p>
+      <p className="info_item">Images by pch.vector on Freepik</p>
+      <p className="info_item">Images by mrsiraphol on Freepik</p>
+      <p className="info_item">Images by Vectonauta on Freepik</p>
+      <p className="info_item">Images by artbutenkov on Freepik</p>
+      <p className="info_item">Images by Racool_studio on Freepik</p>
+      <p className="info_item">Images by ededchechine on Freepik</p>
+      <p className="info_item">Images by chandlervid85 on Freepik</p>
+      <p className="info_item">Images by azerbaijan_stockers on Freepik</p>
+      <p className="info_item">Images by asier_relampagoestudio on Freepik</p>
     </div>
   );
 };
